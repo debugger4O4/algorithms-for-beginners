780358bcd6751a3a3bf4462feca60e9c4eaed361
Kirill <seeraydary@gmail.com>
2024-09-18 13:16:47 +0300

    02-exploring_the_labyrinths.02-factorial-function

---
+++ 02-exploring_the_labyrinths/02-factorial-function/code/Factorial.java
@@ -0,0 +1,10 @@
+public class Factorial {
+
+    public static int getFactorial(int n) {
+        if (n == 0) {
+            return 1;
+        } else {
+            return n * getFactorial(n - 1);
+        }
+    }
+}
