b7794d7fee8433271600d891a759fffa2bea631d
Kirill <seeraydary@gmail.com>
2024-09-18 13:58:20 +0300

    02-exploring_the_labyrinths.01-depth-first-search-over-a-recursive-graph. Codestyle

--- 02-exploring_the_labyrinths/01-depth-first-search-over-a-recursive-graph/code/DepthFirstSearchOverARecursiveGraph.java
+++ 02-exploring_the_labyrinths/01-depth-first-search-over-a-recursive-graph/code/DepthFirstSearchOverARecursiveGraph.java
@@ -10,8 +10,9 @@ public class DepthFirstSearchOverARecursiveGraph {
         adjacencyList = new LinkedList[vertices];
         visited = new boolean[vertices];
 
-        for (int i = 0; i < vertices; i++)
+        for (int i = 0; i < vertices; i++) {
             adjacencyList[i] = new LinkedList<>();
+        }
     }
 
     // Добавление ребер
@@ -25,8 +26,9 @@ public class DepthFirstSearchOverARecursiveGraph {
         System.out.print(vertex + " ");
 
         for (int adj : adjacencyList[vertex]) {
-            if (!visited[adj])
+            if (!visited[adj]) {
                 getDFS(adj);
+            }
         }
     }
 }
