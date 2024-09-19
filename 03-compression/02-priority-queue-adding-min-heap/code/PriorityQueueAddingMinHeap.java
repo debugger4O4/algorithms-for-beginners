6318c197e7a0196513685912691c31817ba6d15c
Kirill <seeraydary@gmail.com>
2024-09-19 18:32:08 +0300

    03-compression.02-priority-queue-adding-min-heap

---
+++ 03-compression/02-priority-queue-adding-min-heap/code/PriorityQueueAddingMinHeap.java
@@ -0,0 +1,8 @@
+import java.util.PriorityQueue;
+
+public class PriorityQueueAddingMinHeap {
+
+    public static void insertInPq(PriorityQueue<Integer> pq, Integer c) {
+
+    }
+}
---
+++ 03-compression/02-priority-queue-adding-min-heap/code/PriorityQueueAddingMinHeapTest.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test;
+
+public class PriorityQueueAddingMinHeapTest {
+
+    @Test
+    void test() {
+    }
+
+}
