ac75192fade03042baeb7a2a8c6eebf27840f503
Kirill <seeraydary@gmail.com>
2024-09-19 08:36:15 +0300

    02-exploring_the_labyrinths.04-depth-first-search-over-a-graph-using-a-stack-without-duplicates

---
+++ 02-exploring_the_labyrinths/04-depth-first-search-over-a-graph-using-a-stack-without-duplicates/code/DepthFirstSearchOverAGraphUsingAStackWithoutDuplicates.java
@@ -0,0 +1,44 @@
+import java.util.LinkedList;
+import java.util.Stack;
+
+public class DepthFirstSearchOverAGraphUsingAStackWithoutDuplicates {
+
+    private final LinkedList<Integer>[] adjacencyList;
+    private final int V;
+
+    // Создание графа
+    DepthFirstSearchOverAGraphUsingAStackWithoutDuplicates(int vertices) {
+        adjacencyList = new LinkedList[vertices];
+        for (int i = 0; i < vertices; i++) {
+            adjacencyList[i] = new LinkedList<>();
+        }
+        this.V = vertices;
+    }
+
+    // Добавление ребер
+    void addEdge(int src, int dest) {
+        adjacencyList[src].add(dest);
+    }
+
+    // Алгоритм
+    public boolean[] getStackWithoutDuplicatesDFS(int vertex) {
+        Stack<Integer> S = new Stack<>();
+        // По дефолту заполняется false'ами
+        boolean[] visited = new boolean[V];
+        boolean[] inStack = new boolean[V];
+        S.push(vertex);
+        inStack[vertex] = true;
+        while (!S.empty()) {
+            int c = S.pop();
+            inStack[c] = false;
+            visited[c] = true;
+            for (int v : adjacencyList[c]) {
+                if (!visited[v] && !inStack[v]) {
+                    S.push(v);
+                    inStack[v] = true;
+                }
+            }
+        }
+        return visited;
+    }
+}
---
+++ 02-exploring_the_labyrinths/04-depth-first-search-over-a-graph-using-a-stack-without-duplicates/code/DepthFirstSearchOverAGraphUsingAStackWithoutDuplicatesTest.java
@@ -0,0 +1,48 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class DepthFirstSearchOverAGraphUsingAStackWithoutDuplicatesTest {
+
+    @Test
+    void test() {
+        DepthFirstSearchOverAGraphUsingAStackWithoutDuplicates graph =
+                new DepthFirstSearchOverAGraphUsingAStackWithoutDuplicates(8);
+
+        graph.addEdge(0, 1);
+        graph.addEdge(0, 2);
+        graph.addEdge(0, 3);
+        graph.addEdge(1, 4);
+        graph.addEdge(1, 0);
+        graph.addEdge(4, 1);
+        graph.addEdge(4, 5);
+        graph.addEdge(5, 4);
+        graph.addEdge(5, 6);
+        graph.addEdge(5, 7);
+        graph.addEdge(5, 3);
+        graph.addEdge(2, 0);
+        graph.addEdge(3, 5);
+        graph.addEdge(3, 0);
+        graph.addEdge(7, 5);
+        graph.addEdge(6, 5);
+
+        boolean[] answer = {true, true, true, true, true, true, true, true};
+
+        assertTrue(
+                answer.length == graph.getStackWithoutDuplicatesDFS(0).length
+                && Arrays.equals(answer, graph.getStackWithoutDuplicatesDFS(0))
+        );
+        /* graph.getStackDFS(0):
+                [0]
+                [1, 2, 3]
+                [1, 2, 5]
+                [1, 2, 4, 6, 7]
+                [1, 2, 4, 6]
+                [1, 2, 4]
+                [1, 2]
+                [1]
+         */
+    }
+}
