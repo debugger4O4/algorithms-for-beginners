680555b5aca26464e20d96671d1bcb49ca3a5a9f
Kirill <seeraydary@gmail.com>
2024-09-20 14:03:59 +0300

    03-compression.02-priority-queue-adding-min-heap

--- 03-compression/02-priority-queue-adding-min-heap/code/PriorityQueueAddingMinHeap.java
+++ 03-compression/02-priority-queue-adding-min-heap/code/PriorityQueueAddingMinHeap.java
@@ -1,8 +1,17 @@
-import java.util.PriorityQueue;
-
 public class PriorityQueueAddingMinHeap {
 
-    public static void insertInPq(PriorityQueue<Integer> pq, Integer c) {
-
+    // Алгоритм
+    public static void insertInPq(PriorityQueueAddingMinHeapTest.MinHeap pq, Integer c) {
+        int root = 0;
+        System.out.println(pq.heap);
+        pq.heap.add(c);
+        System.out.println(pq.heap);
+        int currentIndex = pq.heap.size() - 1;
+        while (currentIndex != root && pq.heap.get(currentIndex) < pq.heap.get(pq.parent(currentIndex))) {
+            int p = pq.parent(currentIndex);
+            pq.exchange(currentIndex, p);
+            currentIndex = pq.parent(currentIndex);
+            System.out.println(pq.heap);
+        }
     }
 }
