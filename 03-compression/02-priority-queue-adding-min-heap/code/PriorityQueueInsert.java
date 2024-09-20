4c796645ad6d1690d22d0f8db1710b83b818e2cf
Kirill <seeraydary@gmail.com>
2024-09-20 16:01:36 +0300

    Rename variable

--- 03-compression/02-priority-queue-adding-min-heap/code/PriorityQueueInsert.java
+++ 03-compression/02-priority-queue-adding-min-heap/code/PriorityQueueInsert.java
@@ -4,11 +4,11 @@ public class PriorityQueueInsert {
     public static void insertInPq(PriorityQueueInsertTest.MinHeap pq, Integer c) {
         int root = 0;
         pq.heap.add(c);
-        int currentIndex = pq.heap.size() - 1;
-        while (currentIndex != root && pq.heap.get(currentIndex) < pq.heap.get(pq.parent(currentIndex))) {
-            int p = pq.parent(currentIndex);
-            pq.exchange(currentIndex, p);
-            currentIndex = pq.parent(currentIndex);
+        c = pq.heap.size() - 1;
+        while (c != root && pq.heap.get(c) < pq.heap.get(pq.parent(c))) {
+            int p = pq.parent(c);
+            pq.exchange(c, p);
+            c = pq.parent(c);
         }
     }
 }
