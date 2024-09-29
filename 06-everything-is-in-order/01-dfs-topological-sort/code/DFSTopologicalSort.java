1524929d789dee0cc4da2a2cfc2af03d48b3463d
Kirill <seeraydary@gmail.com>
2024-09-29 16:20:08 +0300

    06-everything-is-in-order.01-dfs-topological-sort

--- 06-everything-is-in-order/01-dfs-topological-sort/code/DFSTopologicalSort.java
+++ 06-everything-is-in-order/01-dfs-topological-sort/code/DFSTopologicalSort.java
@@ -1,3 +1,21 @@
+import java.util.List;
+import java.util.Stack;
+
 public class DFSTopologicalSort {
 
+    // Алгоритм
+    public static void dfsTopologicalSort(
+            List<List<Integer>> adj, int node,
+            boolean[] visited,
+            Stack<Integer> stack
+    ) {
+        visited[node] = true;
+        for (int i : adj.get(node)) {
+            if (!visited[i])
+                dfsTopologicalSort(adj, i, visited, stack);
+        }
+
+        // Поместить текущую вершину в стек, в котором хранится результат
+        stack.push(node);
+    }
 }
